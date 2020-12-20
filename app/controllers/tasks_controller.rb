345f138b761c1d12ee5e3039a7d8e7a7cfb7a4e7
taishin.fukuyama <taishin.f.0828@icloud.com>
2020-12-20 13:10:12 +0000

    add user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,20 @@
 class TasksController < ApplicationController
     before_action :set_task, only: [:show, :edit, :update, :destroy]
+    before_action :require_user_logged_in
     
     def index
-        @tasks = Task.all
+        @tasks = current_user.tasks.order(id: :desc)
     end
     
     def show
     end
     
     def new
-        @task = Task.new
+        @task = current_user.tasks.build
     end
     
     def create
-        @task = Task.new(task_params)
+        @task = current_user.tasks.build(task_params)
     
         if @task.save
           flash[:success] = 'Task が正常に投稿されました'
@@ -47,7 +48,7 @@ class TasksController < ApplicationController
     private
     
     def set_task
-        @task = Task.find(params[:id])
+        @task = current_user.tasks.find(params[:id])
     end
     
     def task_params
